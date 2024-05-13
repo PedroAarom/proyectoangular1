536a7c6a05a867931619acfeb3caa0cab5ec516d
EC2 Default User <ec2-user@ip-172-31-16-245.ec2.internal>
2024-05-13 18:48:21 +0000

    mensaje

---
+++ src/app/delete-item/delete-item.component.ts
@@ -0,0 +1,27 @@
+/*import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-item',
+  templateUrl: './delete-item.component.html',
+  styleUrl: './delete-item.component.css'
+})
+export class DeleteItemComponent {
+
+}*/
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+ 
+@Component({
+  selector: 'app-delete-item',
+  templateUrl: './delete-item.component.html',
+  styleUrls: ['./delete-item.component.css']
+})
+export class DeleteItemComponent {
+  @Input() item: any;
+  @Output() delete = new EventEmitter<any>();
+ 
+  deleteItem(): void {
+    this.delete.emit(this.item);
+  }
+}
+
+
